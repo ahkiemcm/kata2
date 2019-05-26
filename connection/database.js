1b9d22a352d418b30f65acbd4d872f3b4460e15e
Ahkiem Crenshaw <axc@ahkiems-mbp.attlocal.net>
2019-05-25 23:02:40 -0400

    #4: Connected succesfully to PostgreSQL.

--- connection/database.js
+++ connection/database.js
@@ -1,40 +1,41 @@
 const { Pool } = require("pg");
 
-const CONNECTION_STRING =  process.env.DATABASE_URL || `postgresql://postgres@localhost:5432/weather_db`
-const SSL = process.env.NODE_ENV === 'production'
+const CONNECTION_STRING =
+  process.env.DATABASE_URL || `postgresql://postgres@localhost:5432/weather_db`;
+const SSL = process.env.NODE_ENV === "production";
 
 class Database {
   constructor() {
-      this._pool = new Pool({
-          connectionPool = CONNECTION_STRING,
-          ssl = SSL
-      });
+    this._pool = new Pool({
+      connectionString: CONNECTION_STRING,
+      ssl: SSL
+    });
 
-      this._pool.on(`error`, (err, client) => {
-          console.error(`Unexpected error on idle PostgreSQL client.`, err);
-          process.exit(-1);
-      })
+    this._pool.on(`error`, (err, client) => {
+      console.error(`Unexpected error on idle PostgreSQL client.`, err);
+      process.exit(-1);
+    });
   }
 
-  query() {
-      this._pool.connect((err, client, done) => {
-          if(err) throw err;
-          const params = arg.length === 2 ? args[0]:[];
-          const callback = arg.length === 1 ? args[0]:[1] 
+  query(query, ...args) {
+    this._pool.connect((err, client, done) => {
+      if (err) throw err;
+      const params = args.length === 2 ? args[0] : [];
+      const callback = args.length === 1 ? args[0] : [1];
 
-          client.query(query, params, (err, res) => {
-              done();
-              if(err) {
-                  console.log(err.stack);
-                  return callback({error: 'Database error.'}, null);
-              }
-              callback({}, res.rows);
-          });
+      client.query(query, params, (err, res) => {
+        done();
+        if (err) {
+          console.log(err.stack);
+          return callback({ error: "Database error." }, null);
+        }
+        callback({}, res.rows);
       });
+    });
   }
 
   end() {
-      this._pool.end()
+    this._pool.end();
   }
 }
 
--- connection/server.js
+++ connection/server.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = ('cookie-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = "cookie-parser";
+const path = require("path");
+
+const db = require("./database");
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+  console.log(`Server listening on port ${PORT}`);
+});
+
+db.query("SELECT NOW()", (err, res) => {
+  if (err.error) {
+    return console.log(err.error);
+  } else {
+    console.log(`PostgreSQL connected: ${res[0].now}.`);
+  }
+});
 
 module.exports = app;
